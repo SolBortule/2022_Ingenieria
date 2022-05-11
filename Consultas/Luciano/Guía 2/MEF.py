7023c5ed87e37784e3efc00f491c021628afd045
Chuleno <lucianorielo98@gmail.com>
2022-05-11 19:02:58 -0300

    Pude generalizar la funcion que resolvia el ejercicio 2 para resolver el 3. También hice que esta función me devuelva las tensiones en cada una de las barras en las que partí al elemento. Por último hice un vector que contiene las tensiones en cada uno de los nodos para poder graficarla

--- Consultas/Luciano/Guía 2/MEF.py
+++ Consultas/Luciano/Guía 2/MEF.py
@@ -65,26 +65,28 @@ def Kglobal_barra(MN, MC, E, A, glxn):
     """
     Ke = {}  # diccionario para acumular todas las K_elementales
     Nn = MN.shape[0]  # cantidad de nodos
-    Ne = MC.shape[0]  # cantidad de elementos
+    Ne, Nnxe = MC.shape  # cantidad de elementos
     K = np.zeros([glxn*Nn,glxn*Nn])
     archivo = 'Matrices.txt'  # Creo archivo para guardar todas las matrices elementales
     with open(archivo,'w') as f:  # la f es como un alias apra el archivo que acabo de abrir
         f.write('Matrices Elementales\n ===============')
 
     for e in range(Ne):
-        Ke[e] = Kel_barra(MN, MC, E[e], A[e], e)
+        if glxn == 1:
+            Ke[e] = np.array([[1,-1],[-1,1]])*A*E/(MN[-1]/MC.shape[0])
+        elif glxn == 2:
+            Ke[e] = Kel_barra(MN, MC, E[e], A[e], e)
         fe = np.abs(Ke[e].max())
         with open('Matrices.txt','a') as f:  # 'a' de agregar
             f.write(f'\nelemento {e}, fe ={fe:4e}\n')
             f.write(f'{Ke[e]/fe}\n')
 
-        for i in range(glxn):
+        for i in range(Nnxe):
             rangoi = np.linspace(i*glxn,(i+1)*glxn-1,glxn).astype(int)
             rangoni = np.linspace(MC[e,i]*glxn,(MC[e, i]+1)*glxn-1,glxn).astype(int)
-            for j in range(glxn):
+            for j in range(Nnxe):
                 rangoj = np.linspace(j*glxn,(j+1)*glxn-1,glxn).astype(int)
                 rangonj = np.linspace(MC[e,j]*glxn,(MC[e, j]+1)*glxn-1,glxn).astype(int)
-
                 K[np.ix_(rangoni,rangonj)] += Ke[e][np.ix_(rangoi,rangoj)]
     
     fe = np.abs(K.max())
@@ -97,3 +99,56 @@ def Kglobal_barra(MN, MC, E, A, glxn):
 def vector_complemento(s, MN, glxn):
     r = np.array([i for i in range(glxn*MN.shape[0]) if i not in s])
     return r
+
+def dist_uniforme_barra(A, E, C, L, barras, glxn=1, Nnxe=2):
+    '''
+    #
+    Divide un elemento recto en varias barras, devuelve:
+    f, d, Rx
+    f = fuerza aplicada en los nodos de cada barra
+    d = desplazamiento de cada nodo
+    tensiones = tension en cada barra
+    Rx = Fuerza de reacción en el empotramiento
+    
+    Se ingresa
+    A = sección de la barra
+    E = modulo de elasticidad
+    C = constante correspondiente a la función distribución de la carga
+    L = longitud del elemento
+    barras = cantidad de barras en las que partí al elemento
+    #
+    '''
+    MC = np.array([[i, i+1] for i in range(barras)])
+    MN = np.zeros([barras+1]).reshape([-1,1])
+    sum = 0
+    for i in range(barras+1):
+        MN[i] = sum
+        sum += L/barras
+   
+    # Matriz global
+    Kglobal = Kglobal_barra(MN, MC, E, A, glxn)[0]
+    
+    # barra 1
+    Ft = 0.5*C*(L/barras)**2
+    f1x = Ft/3  # como se distribuyen las cargas en cada nodo del elemento 1
+    f2x = 2*Ft/3  # considerar el area del triangulo
+
+    # el resto de las barras
+    f = np.zeros([barras+1]).reshape([-1,1])
+    for i in range(barras):
+        # Fu -> rectangulo dentro del area, aumenta su magnitud Ft para cada barra 
+        Fu = Ft*i
+        # Fuerza en nodos de la barra i = suma de todos los rectangulos (Ft*i) + carga de la primer barra(triangulito) (Ft)
+        f[i:(i+2)] += np.array([[Fu+f1x],[Fu+f2x]])
+
+    d = np.linalg.solve(Kglobal[:barras,:barras],f[:barras])
+    d = np.append(d,np.array([[0]]),0)
+    Rx = Kglobal.dot(d)[-1]-f[-1]  # fuerza de reacción en el empotramiento
+    
+    eps = np.zeros([barras,1])
+    tensiones = np.zeros([barras,1])
+    for i in range(barras):
+        eps[i] = (d[i+1]-d[i])/(L/barras)
+        tensiones[i] = eps[i]*E
+    
+    return f, d, Rx, tensiones
